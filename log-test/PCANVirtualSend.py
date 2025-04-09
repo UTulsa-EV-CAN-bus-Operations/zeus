8b1656db785a6916299ae4da932297ebc6a6e1cd
Kianna <kiannajj11@gmail.com>
2025-04-08 19:32:27 -0500

    Cleanup of file

--- log-test/PCANVirtualSend.py
+++ log-test/PCANVirtualSend.py
@@ -17,15 +17,14 @@ logger = can.Logger(filename)
 notifier1 = can.Notifier(bus1, [print_listener,logger])
 notifier2 = can.Notifier(bus2,[print_listener,logger])
 
-# Send test messages from bus1 to bus2
-for i in range(10):
-    msg = can.Message(arbitration_id=0xc0ffee1, data=[ i, 0, 1, 3, 1, 4, 1], is_extended_id=True)
-    bus1.send(msg)
+# Send messages from a specified bus with an id in the first 
+def sendMessageChain(bus, id):
+    for i in range(10):
+        msg = can.Message(arbitration_id=0xc0ffee, data=[ id, i, 1, 3, 1, 4, 1], is_extended_id=True)
+        bus.send(msg)
 
-# Send test messages from bus2 to bus1
-for i in range(10):
-    msg = can.Message(arbitration_id=0xc0ffee2, data=[ i, 0, 1, 3, 1, 4, 1], is_extended_id=True)
-    bus2.send(msg)
+sendMessageChain(bus1,1)
+sendMessageChain(bus2,2)
 
 # Ensure all messages are logged
 time.sleep(1.0)
