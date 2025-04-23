7be5f17e8cbbfc7687ee311e8de07e8d0a2d50e9
Kianna <kiannajj11@gmail.com>
2025-04-22 23:11:30 -0500

    Initial commit with config screen file created but not implemented

---
+++ zeus-app/ConfigSetup.py
@@ -0,0 +1,80 @@
+import can
+import time
+import datetime
+
+from can import LogReader, MessageSync
+
+from textual import on
+from textual.app import App, ComposeResult
+from textual.containers import Container, Horizontal
+from textual.widgets import Header, Footer, Input, Select, Button
+
+class ReplayConfig():
+    filename = "reverse-isolated.trc"
+    interface = "pcan"
+    channel = "PCAN_USBBUS1"
+    bitrate = 500000
+
+
+class ToolApp(App):
+    
+    CSS_PATH = "ConfigScreen.tcss"
+
+    def __init__(self):
+        super().__init__()
+        self.config = ReplayConfig()
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        yield Container(
+            Horizontal(
+                Select(options=[("pcan", "pcan"), ("virtual", "virtual")], prompt="Select Interface", id="interface"),
+                Select(options=[("PCAN_USBBUS1", "PCAN_USBBUS1"), ("test", "test")], prompt="Select Channel", id="channel"),
+                Select(options=[("500000", "500000")], prompt="Select Bitrate", id="bitrate")
+            ),
+            Button(label="Start", id="start"), 
+            name="busconfig",
+            id="busconfig"
+        )
+        
+    """def on_button_pressed(self, event: Button.Pressed) -> None:
+
+        bus = can.interface.Bus(channel=self.config.channel, interface=self.config.interface, bitrate=self.config.bitrate, receive_own_messages=True)
+
+        fileExtension = ".csv"
+        filename = 'trc-logs\\TextualReplayTESTlog1PACM-{date:%Y-%m-%d_%H-%M-%S}.{extension}'.format(date=datetime.datetime.now(),extension=fileExtension)
+        logger = can.Logger(filename)
+
+        reader = LogReader(self.config.filename)
+
+        notifier = can.Notifier(bus, [logger])
+        
+        for msg in MessageSync(messages=reader):
+            try:
+                if msg.is_error_frame == False:
+                    print(msg + "\n")
+                    bus.send(msg)
+
+            except KeyboardInterrupt:
+                pass
+        
+        time.sleep(1.0)
+        notifier.stop()
+        bus.shutdown()"""
+
+    @on(Select.Changed)
+    def select_changed(self, event: Select.Changed) -> None:
+        select_widget = event.select
+        if(select_widget.id == "interface"):
+            self.config.interface = event.value
+        elif(select_widget.id == "channel"):
+            self.config.channel = event.value
+        else:
+            self.config.bitrate = int(event.value)
+    
+            
+
+if __name__ == "__main__":
+    app = ToolApp()
+    app.run()
